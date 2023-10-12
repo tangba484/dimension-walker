9607a203d36087173097bbbbd9a3465b3ad79812
tangba484 <tangba484@naver.com>
2023-10-12 19:31:59 +0900

    최신화

--- dimension_walker.py
+++ dimension_walker.py
@@ -1,6 +1,7 @@
 import Stock
 
-df = Stock.getStockDf('AAPL')
+ticker = "005930.KS"
+df = Stock.getStockDf(ticker)
 df = Stock.addNasdaq(df)
 df = Stock.addSnp500(df)
 df = Stock.addKrwusd(df)
@@ -8,3 +9,4 @@ df = Stock.addWti(df)
 df = Stock.addGold(df)
 df = Stock.addCsi(df)
 print(df)
+print(df.columns)
